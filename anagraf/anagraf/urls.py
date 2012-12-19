b2122f48d2785c4060709181ace4e890368410ff
root <root@Wolf.(none)>
2012-12-19 11:38:46 +0100

    First view turned on

--- anagraf/anagraf/urls.py
+++ anagraf/anagraf/urls.py
@@ -1,10 +1,14 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url, include
+from django.conf.urls.defaults import *
+
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
+        url(r'^tempocorrente/$', 'registroDati.views.current_datetime'),
 
     # Examples:
     # url(r'^$', 'anagraf.views.home', name='home'),
@@ -14,5 +18,5 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-url(r'^admin/', include(admin.site.urls)),
+        url(r'^admin/', include(admin.site.urls)),
 )
--- anagraf/registroDati/views.py
+++ anagraf/registroDati/views.py
@@ -1,1 +1,11 @@
 # Create your views here.
+
+from django.http import HttpResponse,HttpRequest
+import datetime
+
+#Vista che permette di ritonare la data e l'ora
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
